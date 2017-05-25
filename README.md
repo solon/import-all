642220c4f8619db165563a12bb70a8bdd5ca4de1
Brian Solon <brian9@gmail.com>
2017-05-25 01:05:21 +0100

    Add optional asObject parameter
    - Allows returning an object with module names as keys.

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 import-all
 ----------
 
-given the path to a directory containing javascript files, returns an array of
-all of their default exports
+Given the path to a directory containing JavaScript files, returns an array of
+all of their default exports.
 
 ```javascript
 import importAll from 'import-all';
 const imports = importAll(`${__dirname}/directory`);
+```
+
+Alternatively, by passing an optional `asObject = true` parameter, it returns 
+an object with module names as keys.
+
+```javascript
+import importAll from 'import-all';
+const imports = importAll(`${__dirname}/directory`, true);
 ```
--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 var fs = require('fs');
 var path = require('path');
 
-module.exports = function(importPath) {
-  return fs.readdirSync(importPath)
+module.exports = function(importPath, asObject = false) {
+  return fs
+    .readdirSync(importPath)
     .filter(function(fileName) {
       return fileName.match(/\.js$/);
     })
     .map(function(fileName) {
-      return require(path.join(importPath, fileName));
+      return {
+        name: fileName.split('.js')[0],
+        module: require(path.join(importPath, fileName)),
+      };
     })
-    .filter(function(module) {
-      return module.__esModule
+    .filter(function({ name, module }) {
+      return { name, module: module.__esModule };
     })
-    .map(function(module) {
-      return module.default;
-    });
+    .map(function({ name, module }) {
+      return { name, module: module.default };
+    })
+    .reduce(
+      (accumulator, currentValue, currentIndex) => {
+        asObject
+          ? (accumulator[currentValue.name] = currentValue.module)
+          : accumulator.push(currentValue.module);
+        return accumulator;
+      },
+      asObject ? {} : []
+    ); 
 };
